95b96236064a4a414ebcafcf1fdb747a3e78d8dc
Emile <emile.swarts123@gmail.com>
2013-06-24 09:25:48 +0100

    Remove unused stuff

--- custom/example.zsh
+++ custom/example.zsh
@@ -1,19 +1,5 @@
 # Add yourself some shortcuts to projects you often work on
 
-alias sshfsbrix='sshfs emile@brix.fatbeehive.com:/server/www/ ~/srv/brix/'
-alias sshfshomer='sshfs emile@homer.fatbeehive.com:/server/www/ ~/srv/homer/'
-alias sshfsfalco='sshfs -p 6007 emile@falco.fatbeehive.com:/server/www ~/srv/falco/'
-alias sshfsmicko='sshfs emile@micko.fatbeehive.com:/server/www ~/srv/micko/'
-alias sshfsnearly='sshfs korpzone_emileswarts@ssh.phx.nearlyfreespeech.net:/ ~/srv/nearly/'
-alias sshfsnearlyvim='sshfs korpzone_myvimcommands@ssh.phx.nearlyfreespeech.net:/ ~/srv/nearlyvim/'
-alias sshfstrevor='sshfs emile@192.168.1.64:/emile ~/srv/trevor/'
-
-alias ubrix='fusermount -z -u ~/srv/brix'
-alias uhomer='fusermount -z -u ~/srv/homer'
-alias ufalco='fusermount -z -u ~/srv/falco'
-alias umicko='fusermount -z -u ~/srv/micko'
-alias utrevor='fusermount -z -u ~/srv/trevor'
-
 #gmail accounts
 #korpzone@gmail.com
 alias muttkp='mutt -F ~/.muttrc_kp'
@@ -21,19 +7,12 @@ alias muttkp='mutt -F ~/.muttrc_kp'
 #emile.swarts123@gmail.com
 alias muttem='mutt -F ~/.muttrc_em'
 
-#emilefabeehive@gmail.com
-alias muttemfb='mutt -F ~/.muttrc_fbgmail'
-
 # some more ls aliases
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -al'
 
 #alias ack='ack-grep'
-
-#Firefox
-alias ff='firefox &'
-alias fff='/usr/bin/firefox-4.0 &'
 
 #take notes
 alias note='vim ~/notes/client.txt'
@@ -43,8 +22,9 @@ alias lhomer='~/remoteserv/HOMER'
 alias lbrix='~/remoteserv/BRIX'
 
 #vimrecordsession
-alias vi='vim'
-alias v='vim'
+alias vim='/usr/local/bin/vim'
+alias vi='/usr/local/bin/vim'
+alias v='/usr/local/bin/vim'
 
 #cd
 alias ..='../'
